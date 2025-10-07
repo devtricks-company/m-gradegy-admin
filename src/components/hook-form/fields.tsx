c81c1398affb3a1ec5384b1a200bba81259d484d
dev.team <dev.team@gradegy.com>
2025-10-08 00:12:36 +0330

    part 1

--- src/components/hook-form/fields.tsx
+++ src/components/hook-form/fields.tsx
@@ -1,5 +1,6 @@
 import { RHFTextField } from './rhf-text-field';
 import { RHFFileUpload } from './rhf-file-upload';
+import { RHFSelect } from './rhf-select';
 import { RHFAutocompleteSchoolDistrict } from './rhf-autocomplete-school-district';
 import { RHFAutocompleteUniversity } from './rhf-autocomplete-university';
 import { RHFAutocompleteLeadContact } from './rhf-autocomplete-lead-contact';
@@ -9,6 +10,7 @@ import { RHFAutocompleteLeadContact } from './rhf-autocomplete-lead-contact';
 export const Field = {
   Text: RHFTextField,
   Upload: RHFFileUpload,
+  Select: RHFSelect,
   AutocompleteSchoolDistrict: RHFAutocompleteSchoolDistrict,
   AutocompleteUniversity: RHFAutocompleteUniversity,
   AutocompleteLeadContact: RHFAutocompleteLeadContact,
