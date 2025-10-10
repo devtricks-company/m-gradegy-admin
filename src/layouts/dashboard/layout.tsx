2b522a9efd2790f1140bcb728eb2d16fbeebeff7
dev.team <dev.team@gradegy.com>
2025-10-10 22:01:59 +0330

    save changs

--- src/layouts/dashboard/layout.tsx
+++ src/layouts/dashboard/layout.tsx
@@ -68,7 +68,7 @@ export function DashboardLayout({ sx, children, header, data }: DashboardLayoutP
   const isNavVertical = isNavMini || settings.navLayout === 'vertical';
 
   // Fetch organizations from access control
-  const { data: organizationsData, isSuccess } = useAccessControlControllerListOrganizations();
+  const { data: organizationsData } = useAccessControlControllerListOrganizations();
 
   // Transform organizations to match WorkspacesPopover format
   const workspaces =
@@ -81,167 +81,167 @@ export function DashboardLayout({ sx, children, header, data }: DashboardLayoutP
   return (
     <WorkspaceProvider>
       <LayoutSection
-      /** **************************************
-       * Header
-       *************************************** */
-      headerSection={
-        <HeaderSection
-          layoutQuery={layoutQuery}
-          disableElevation={isNavVertical}
-          slotProps={{
-            toolbar: {
-              sx: {
-                ...(isNavHorizontal && {
-                  bgcolor: 'var(--layout-nav-bg)',
-                  [`& .${iconButtonClasses.root}`]: {
-                    color: 'var(--layout-nav-text-secondary-color)',
-                  },
-                  [theme.breakpoints.up(layoutQuery)]: {
-                    height: 'var(--layout-nav-horizontal-height)',
-                  },
-                }),
+        /** **************************************
+         * Header
+         *************************************** */
+        headerSection={
+          <HeaderSection
+            layoutQuery={layoutQuery}
+            disableElevation={isNavVertical}
+            slotProps={{
+              toolbar: {
+                sx: {
+                  ...(isNavHorizontal && {
+                    bgcolor: 'var(--layout-nav-bg)',
+                    [`& .${iconButtonClasses.root}`]: {
+                      color: 'var(--layout-nav-text-secondary-color)',
+                    },
+                    [theme.breakpoints.up(layoutQuery)]: {
+                      height: 'var(--layout-nav-horizontal-height)',
+                    },
+                  }),
+                },
               },
-            },
-            container: {
-              maxWidth: false,
-              sx: {
-                ...(isNavVertical && { px: { [layoutQuery]: 5 } }),
+              container: {
+                maxWidth: false,
+                sx: {
+                  ...(isNavVertical && { px: { [layoutQuery]: 5 } }),
+                },
               },
-            },
-          }}
-          sx={header?.sx}
-          slots={{
-            topArea: (
-              <Alert severity="info" sx={{ display: 'none', borderRadius: 0 }}>
-                This is an info Alert.
-              </Alert>
-            ),
-            bottomArea: isNavHorizontal ? (
-              <NavHorizontal
-                data={navData}
-                layoutQuery={layoutQuery}
-                cssVars={navColorVars.section}
-              />
-            ) : null,
-            leftArea: (
-              <>
-                {/* -- Nav mobile -- */}
-                <MenuButton
-                  onClick={mobileNavOpen.onTrue}
-                  sx={{
-                    mr: 1,
-                    ml: -1,
-                    [theme.breakpoints.up(layoutQuery)]: { display: 'none' },
-                  }}
-                />
-                <NavMobile
+            }}
+            sx={header?.sx}
+            slots={{
+              topArea: (
+                <Alert severity="info" sx={{ display: 'none', borderRadius: 0 }}>
+                  This is an info Alert.
+                </Alert>
+              ),
+              bottomArea: isNavHorizontal ? (
+                <NavHorizontal
                   data={navData}
-                  open={mobileNavOpen.value}
-                  onClose={mobileNavOpen.onFalse}
+                  layoutQuery={layoutQuery}
                   cssVars={navColorVars.section}
                 />
-                {/* -- Logo -- */}
-                {isNavHorizontal && (
-                  <Logo
+              ) : null,
+              leftArea: (
+                <>
+                  {/* -- Nav mobile -- */}
+                  <MenuButton
+                    onClick={mobileNavOpen.onTrue}
                     sx={{
-                      display: 'none',
-                      [theme.breakpoints.up(layoutQuery)]: { display: 'inline-flex' },
+                      mr: 1,
+                      ml: -1,
+                      [theme.breakpoints.up(layoutQuery)]: { display: 'none' },
                     }}
                   />
-                )}
-                {/* -- Divider -- */}
-                {isNavHorizontal && (
-                  <StyledDivider
-                    sx={{ [theme.breakpoints.up(layoutQuery)]: { display: 'flex' } }}
+                  <NavMobile
+                    data={navData}
+                    open={mobileNavOpen.value}
+                    onClose={mobileNavOpen.onFalse}
+                    cssVars={navColorVars.section}
                   />
-                )}
-                {/* -- Workspace popover -- */}
+                  {/* -- Logo -- */}
+                  {isNavHorizontal && (
+                    <Logo
+                      sx={{
+                        display: 'none',
+                        [theme.breakpoints.up(layoutQuery)]: { display: 'inline-flex' },
+                      }}
+                    />
+                  )}
+                  {/* -- Divider -- */}
+                  {isNavHorizontal && (
+                    <StyledDivider
+                      sx={{ [theme.breakpoints.up(layoutQuery)]: { display: 'flex' } }}
+                    />
+                  )}
+                  {/* -- Workspace popover -- */}
 
-                <WorkspacesPopover
-                  data={workspaces}
-                  sx={{ color: 'var(--layout-nav-text-primary-color)' }}
-                />
-              </>
-            ),
-            rightArea: (
-              <Box display="flex" alignItems="center" gap={{ xs: 0, sm: 0.75 }}>
-                {/* -- Searchbar -- */}
-                <Searchbar data={navData} />
-                {/* -- Language popover -- */}
-                <LanguagePopover
-                  data={[
-                    { value: 'en', label: 'English', countryCode: 'GB' },
-                    { value: 'fr', label: 'French', countryCode: 'FR' },
-                    { value: 'vi', label: 'Vietnamese', countryCode: 'VN' },
-                    { value: 'cn', label: 'Chinese', countryCode: 'CN' },
-                    { value: 'ar', label: 'Arabic', countryCode: 'SA' },
-                  ]}
-                />
-                {/* -- Notifications popover -- */}
-                <NotificationsDrawer data={_notifications} />
-                {/* -- Contacts popover -- */}
-                <ContactsPopover data={_contacts} />
-                {/* -- Settings button -- */}
-                <SettingsButton />
-                {/* -- Account drawer -- */}
-                <AccountDrawer data={_account} />
-              </Box>
-            ),
-          }}
-        />
-      }
-      /** **************************************
-       * Sidebar
-       *************************************** */
-      sidebarSection={
-        isNavHorizontal ? null : (
-          <NavVertical
-            data={navData}
-            isNavMini={isNavMini}
-            layoutQuery={layoutQuery}
-            cssVars={navColorVars.section}
-            onToggleNav={() =>
-              settings.onUpdateField(
-                'navLayout',
-                settings.navLayout === 'vertical' ? 'mini' : 'vertical'
-              )
-            }
+                  <WorkspacesPopover
+                    data={workspaces}
+                    sx={{ color: 'var(--layout-nav-text-primary-color)' }}
+                  />
+                </>
+              ),
+              rightArea: (
+                <Box display="flex" alignItems="center" gap={{ xs: 0, sm: 0.75 }}>
+                  {/* -- Searchbar -- */}
+                  <Searchbar data={navData} />
+                  {/* -- Language popover -- */}
+                  <LanguagePopover
+                    data={[
+                      { value: 'en', label: 'English', countryCode: 'GB' },
+                      { value: 'fr', label: 'French', countryCode: 'FR' },
+                      { value: 'vi', label: 'Vietnamese', countryCode: 'VN' },
+                      { value: 'cn', label: 'Chinese', countryCode: 'CN' },
+                      { value: 'ar', label: 'Arabic', countryCode: 'SA' },
+                    ]}
+                  />
+                  {/* -- Notifications popover -- */}
+                  <NotificationsDrawer data={_notifications} />
+                  {/* -- Contacts popover -- */}
+                  <ContactsPopover data={_contacts} />
+                  {/* -- Settings button -- */}
+                  <SettingsButton />
+                  {/* -- Account drawer -- */}
+                  <AccountDrawer data={_account} />
+                </Box>
+              ),
+            }}
           />
-        )
-      }
-      /** **************************************
-       * Footer
-       *************************************** */
-      footerSection={null}
-      /** **************************************
-       * Style
-       *************************************** */
-      cssVars={{
-        ...navColorVars.layout,
-        '--layout-transition-easing': 'linear',
-        '--layout-transition-duration': '120ms',
-        '--layout-nav-mini-width': '88px',
-        '--layout-nav-vertical-width': '300px',
-        '--layout-nav-horizontal-height': '64px',
-        '--layout-dashboard-content-pt': theme.spacing(1),
-        '--layout-dashboard-content-pb': theme.spacing(8),
-        '--layout-dashboard-content-px': theme.spacing(5),
-      }}
-      sx={{
-        [`& .${layoutClasses.hasSidebar}`]: {
-          [theme.breakpoints.up(layoutQuery)]: {
-            transition: theme.transitions.create(['padding-left'], {
-              easing: 'var(--layout-transition-easing)',
-              duration: 'var(--layout-transition-duration)',
-            }),
-            pl: isNavMini ? 'var(--layout-nav-mini-width)' : 'var(--layout-nav-vertical-width)',
+        }
+        /** **************************************
+         * Sidebar
+         *************************************** */
+        sidebarSection={
+          isNavHorizontal ? null : (
+            <NavVertical
+              data={navData}
+              isNavMini={isNavMini}
+              layoutQuery={layoutQuery}
+              cssVars={navColorVars.section}
+              onToggleNav={() =>
+                settings.onUpdateField(
+                  'navLayout',
+                  settings.navLayout === 'vertical' ? 'mini' : 'vertical'
+                )
+              }
+            />
+          )
+        }
+        /** **************************************
+         * Footer
+         *************************************** */
+        footerSection={null}
+        /** **************************************
+         * Style
+         *************************************** */
+        cssVars={{
+          ...navColorVars.layout,
+          '--layout-transition-easing': 'linear',
+          '--layout-transition-duration': '120ms',
+          '--layout-nav-mini-width': '88px',
+          '--layout-nav-vertical-width': '300px',
+          '--layout-nav-horizontal-height': '64px',
+          '--layout-dashboard-content-pt': theme.spacing(1),
+          '--layout-dashboard-content-pb': theme.spacing(8),
+          '--layout-dashboard-content-px': theme.spacing(5),
+        }}
+        sx={{
+          [`& .${layoutClasses.hasSidebar}`]: {
+            [theme.breakpoints.up(layoutQuery)]: {
+              transition: theme.transitions.create(['padding-left'], {
+                easing: 'var(--layout-transition-easing)',
+                duration: 'var(--layout-transition-duration)',
+              }),
+              pl: isNavMini ? 'var(--layout-nav-mini-width)' : 'var(--layout-nav-vertical-width)',
+            },
           },
-        },
-        ...sx,
-      }}
-    >
-      <Main isNavHorizontal={isNavHorizontal}>{children}</Main>
-    </LayoutSection>
+          ...sx,
+        }}
+      >
+        <Main isNavHorizontal={isNavHorizontal}>{children}</Main>
+      </LayoutSection>
     </WorkspaceProvider>
   );
 }
