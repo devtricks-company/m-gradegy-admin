e915a612611b37bb571106eab3a9e064afd8a261
dev.team <dev.team@gradegy.com>
2025-10-12 23:04:58 +0330

    change endtime

--- src/components/hook-form/rhf-date-picker.tsx
+++ src/components/hook-form/rhf-date-picker.tsx
@@ -1,12 +1,14 @@
 import type { DatePickerProps } from '@mui/x-date-pickers/DatePicker';
+import type { Dayjs } from 'dayjs';
 
+import dayjs from 'dayjs';
 import { useFormContext, Controller } from 'react-hook-form';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { FormHelperText } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
-type Props = DatePickerProps<Date> & {
+type Props = DatePickerProps<Dayjs> & {
   name: string;
   helperText?: React.ReactNode;
 };
@@ -18,25 +20,31 @@ export function RHFDatePicker({ name, helperText, ...other }: Props) {
     <Controller
       name={name}
       control={control}
-      render={({ field, fieldState: { error } }) => (
-        <div>
-          <DatePicker
-            {...field}
-            value={field.value || null}
-            onChange={(newValue) => {
-              field.onChange(newValue);
-            }}
-            slotProps={{
-              textField: {
-                fullWidth: true,
-                error: !!error,
-                helperText: error ? error?.message : helperText,
-              },
-            }}
-            {...other}
-          />
-        </div>
-      )}
+      render={({ field, fieldState: { error } }) => {
+        // Convert Date to Dayjs for display, or null if no value
+        const displayValue = field.value ? dayjs(field.value) : null;
+
+        return (
+          <div>
+            <DatePicker
+              value={displayValue}
+              onChange={(newValue: Dayjs | null) => {
+                // Convert Dayjs to native Date object for form storage, or undefined if null
+                const dateValue = newValue?.isValid() ? newValue.toDate() : undefined;
+                field.onChange(dateValue);
+              }}
+              slotProps={{
+                textField: {
+                  fullWidth: true,
+                  error: !!error,
+                  helperText: error ? error?.message : helperText,
+                },
+              }}
+              {...other}
+            />
+          </div>
+        );
+      }}
     />
   );
 }
--- src/components/hook-form/rhf-time-picker.tsx
+++ src/components/hook-form/rhf-time-picker.tsx
@@ -1,12 +1,14 @@
 import type { TimePickerProps } from '@mui/x-date-pickers/TimePicker';
+import type { Dayjs } from 'dayjs';
 
+import dayjs from 'dayjs';
 import { useFormContext, Controller } from 'react-hook-form';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { FormHelperText } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
-type Props = TimePickerProps<Date> & {
+type Props = TimePickerProps<Dayjs> & {
   name: string;
   helperText?: React.ReactNode;
 };
@@ -18,25 +20,31 @@ export function RHFTimePicker({ name, helperText, ...other }: Props) {
     <Controller
       name={name}
       control={control}
-      render={({ field, fieldState: { error } }) => (
-        <div>
-          <TimePicker
-            {...field}
-            value={field.value || null}
-            onChange={(newValue) => {
-              field.onChange(newValue);
-            }}
-            slotProps={{
-              textField: {
-                fullWidth: true,
-                error: !!error,
-                helperText: error ? error?.message : helperText,
-              },
-            }}
-            {...other}
-          />
-        </div>
-      )}
+      render={({ field, fieldState: { error } }) => {
+        // Convert Date to Dayjs for display, or null if no value
+        const displayValue = field.value ? dayjs(field.value) : null;
+
+        return (
+          <div>
+            <TimePicker
+              value={displayValue}
+              onChange={(newValue: Dayjs | null) => {
+                // Convert Dayjs to native Date object for form storage, or undefined if null
+                const dateValue = newValue?.isValid() ? newValue.toDate() : undefined;
+                field.onChange(dateValue);
+              }}
+              slotProps={{
+                textField: {
+                  fullWidth: true,
+                  error: !!error,
+                  helperText: error ? error?.message : helperText,
+                },
+              }}
+              {...other}
+            />
+          </div>
+        );
+      }}
     />
   );
 }
